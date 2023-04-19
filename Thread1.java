ab14dec6457e3e48da7a08e08d4e921963632389
Excusemeow <782084435@qq.com>
2023-04-19 23:34:27 +0800

    更新 Thread1.java

--- Thread1.java
+++ Thread1.java
@@ -55,7 +55,7 @@
 // 继承Thread类的方法最简单，但是由于Java只支持单继承，所以如果一个类已经继承了其他类，就不能再继承Thread类了。而且这种方法不能实现多个线程共享同一个资源的目的。
 // 实现Runnable接口的方法更灵活，可以避免单继承的局限性，也可以实现多个线程共享同一个资源的目的。但是这种方法不能让线程有返回值，也不能让线程抛出异常。
 // 实现Callable接口的方法最强大，可以让线程有返回值，也可以让线程抛出异常。但是这种方法相对复杂，需要借助FutureTask类来获取返回值。
-// b --------------------------线程控制
+// b --------------------------Thread线程控制
 //o 线程中常用的方法有以下几种：
 //g start ()：启动一个新线程，新线程会执行 run () 方法。调用 start () 方法后，该线程进入就绪状态，等待调度器分配 CPU 资源。
 //g run ()：定义线程要执行的任务，可以直接调用，也可以在子类中重写。直接调用 run () 方法不会启动新线程，只是在当前线程中执行 run () 方法。
@@ -68,6 +68,10 @@
 //g join ()：让当前线程等待另一个线程结束，相当于在当前线程中调用另一个线程的 run () 方法。
 //g yield ()：让当前线程让出 CPU 时间片，重新进入就绪状态，等待调度器重新分配 CPU 资源。该方法是静态的，可以直接用 Thread.yield () 调用。调用 yield () 方法并不会让线程进入阻塞状态，也不会释放对象锁，只是给其他同级线程一个执行的机会。放入就绪队列尾部
 //g isAlive ()：判断当前线程是否存活，即是否处于新建或终止状态。该方法是非静态的，需要用 Thread 对象来调用。返回值是一个布尔值，如果线程还活着，返回 true，否则返回 false。
+
+//g interrupt()：中断线程，设置线程的中断标志位。
+//g isInterrupted()：判断线程是否被中断。
+
 // o 线程优先级调度
 // 线程优先级方法是 Thread 类中的一些方法，用于设置和获取线程的优先级。线程的优先级是一个整数，取值范围是 1-10，其中 1 是最低优先级，10 是最高优先级，5 是默认优先级。线程的优先级可以影响线程调度的顺序，但不是绝对的，具体还取决于操作系统的实现。
 //g yield ()：让当前线程让出 CPU 时间片，重新进入就绪状态，等待调度器重新分配 CPU 资源。该方法是静态的，可以直接用 Thread.yield () 调用。调用 yield () 方法并不会让线程进入阻塞状态，也不会释放对象锁，只是给其他线程一个执行的机会。放入就绪队列尾部
@@ -75,9 +79,14 @@
 //g getPriority ()：获取线程的优先级，返回值是一个整数。
 // setDefaultUncaughtExceptionHandler (Thread.UncaughtExceptionHandler eh)：设置线程的默认未捕获异常处理器，参数 eh 是一个 Thread.UncaughtExceptionHandler 对象，用于处理线程在运行过程中发生的未捕获异常。
 // getDefaultUncaughtExceptionHandler ()：获取线程的默认未捕获异常处理器，返回值是一个 Thread.UncaughtExceptionHandler 对象。
+//g 线程信息
+// setName(String name)：设置线程的名称。如果不设置线程的名称，线程会自动生成一个默认的名称，例如"Thread-0"、"Thread-1"等。
+// getName()：获取线程的名称。
+// getState()：获取线程的状态，有新建、就绪、运行、阻塞、死亡等状态。
+//no isDaemon()：判断线程是否是守护线程。
+//no setDaemon(boolean on)：设置线程是否为守护线程。
 // b --------------------------线程的互斥
 // 在Java中，可以使用关键字synchronized和lock机制来实现线程的互斥
-
 //o synchronized关键字：它可以用来修饰方法或代码块。当线程进入被synchronized修饰的代码块时，它会试图获得锁。如果锁没有被其他线程占用，则该线程获得锁，并且可以执行代码。如果锁已经被其他线程占用，则该线程会被阻塞，直到锁被释放。
 /**
  * g 同步代码块使用synchronized关键字来锁定一个方法，可以使该代码块在任意时刻只能被一个线程执行
@@ -92,8 +101,15 @@
  * w synchronized(lock){// 这个代码块在任意时刻只能被一个线程执行// ... }
  * w ...}
  */
-// lock机制：它可以更加灵活地控制锁的获取和释放。Lock接口定义了lock()和unlock()方法，分别用于获取和释放锁。使用lock机制可以避免synchronized关键字可能出现的一些问题，例如死锁、无法中断等。
 
+// lock机制：它可以更加灵活地控制锁的获取和释放。Lock接口定义了lock()和unlock()方法，分别用于获取和释放锁。使用lock机制可以避免synchronized关键字可能出现的一些问题，例如死锁、无法中断等。
+/**Lock是一个Java接口，它定义了一些用于实现线程同步的方法，例如lock()，unlock()，tryLock()等。
+ * Lock接口比synchronized关键字提供了更灵活和更强大的锁功能，例如可以支持公平性，可中断性，超时等。
+ * Lock接口有多个实现类，例如ReentrantLock，ReentrantReadWriteLock，StampedLock等。使用Lock接口时，需要注意以下几点：
+Lock接口不是自动释放锁的，需要在finally语句块中手动调用unlock()方法来释放锁，否则可能会导致死锁或内存泄漏。
+Lock接口可以配合Condition接口来实现线程间的等待和通知机制，类似于Object类的wait()和notify()方法。
+Lock接口可以通过设置fairness属性来控制锁的公平性，如果为true，则按照线程请求锁的顺序来分配锁；如果为false，则不保证任何顺序。
+*/
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
@@ -101,14 +117,14 @@ public class Thread1 {
     public static void main(String[] args) {
         // 定义一个共享的锁对象
         Object lock = new Object();
-        
-        // 创建一个MyThread类的线程
+
+        // -------------------------创建一个MyThread类的线程
         MyThread thread = new MyThread(lock);
-        // 创建一个MyRunnable类的线程
+        // -------------------------创建一个MyRunnable类的线程
         MyRunnable runnable = new MyRunnable();
         Thread thread1 = new Thread(runnable);
-        
-        // 创建一个MyCallable类的线程
+
+        // ----------------------------创建一个MyCallable类的线程
         // 在main 方法中将 MyThread 线程的 ID 传递给 MyCallable类的构造方法，这样就可以根据这个 ID 来唤醒线程。
         MyCallable callable = new MyCallable(lock, thread.getId());// Thread.currentThread().getId()替代过时getId()
         FutureTask<String> task = new FutureTask<>(callable);
@@ -137,16 +153,16 @@ class MyThread extends Thread {
             for (int i = 0; 5 > i; i++) {
                 System.out.println("thread t1  " + i);
                 // 在synchronized代码块中调用wait方法
-                
-                    if (i == 0) {
-                        try {
-                            System.out.println("thread t1  等待");
-                            lock.wait(); // 释放锁，并等待
-                        } catch (InterruptedException e) {// 需要捕获interrupted
-                            e.printStackTrace();
-                        }
+
+                if (i == 0) {
+                    try {
+                        System.out.println("thread t1  等待");
+                        lock.wait(); // 释放锁，并等待
+                    } catch (InterruptedException e) {// 需要捕获interrupted
+                        e.printStackTrace();
                     }
-                
+                }
+
             }
         }
     }
@@ -174,7 +190,7 @@ class MyCallable implements Callable<String> {
         this.threadId = threadId;// MyCallable需要知道要唤醒哪个线程的ID，才能在正确的锁对象上调用notify方法，唤醒指定的线程。
     }
 
-    public  String call() {
+    public String call() {
         // 线程执行逻辑
         synchronized (lock) {
             for (int i = 0; 5 > i; i++) {
@@ -196,7 +212,154 @@ class MyCallable implements Callable<String> {
                     }
                 }
             }
-        }//
+        } //
         return "result";
+    }
+}
+
+// g 设置线程优先级的示例 线程优先级的高低并不一定决定了线程的执行顺序，这还取决于操作系统和虚拟机的调度策略。
+// 定义一个自定义线程类，继承Thread类
+class MyT extends Thread {
+    // 重写run方法
+
+    public void run() {
+        // 打印当前线程的名称和优先级
+        System.out.println(this.getName() + " : " + this.getPriority());
+    }
+}
+
+class ThreadTest {
+    public static void main(String[] args) {
+        // o 创建一个主线程对象 currentThread() 是 Thread 类的一个静态方法，用于获取当前执行线程的引用。//非必要
+        // 程序启动时，会自动创建一个主线程（Main Thread），也就是启动程序的线程，该线程也可以通过 currentThread() 方法来获取。
+        Thread mainThread = Thread.currentThread();
+        // 设置主线程的优先级为6
+        mainThread.setPriority(6);
+        // 打印主线程的名称和优先级
+        System.out.println(mainThread.getName() + " : " + mainThread.getPriority());
+
+        // 创建两个自定义线程对象
+        MyT t1 = new MyT();
+        MyT t2 = new MyT();
+        MyT t3 = new MyT();
+        // 设置t1的优先级为最高（10）
+        t1.setPriority(Thread.MAX_PRIORITY);// Thread的属性
+        // 设置t2的优先级为最低（1）
+        t2.setPriority(Thread.MIN_PRIORITY);
+        // 启动两个自定义线程
+        t1.start();
+        t2.start();
+        t3.start();
+        new MyT().start();// new 类对象直接启动
+    }
+}
+// g ------------------使用wait()和notify()方法的示例代码。
+// 代码是一个简单的生产者消费者问题，其中有一个共享的缓冲区，生产者线程向缓冲区中放入数据，消费者线程从缓冲区中取出数据。为了避免缓冲区溢出或空闲，生产者线程和消费者线程需要进行同步，即当缓冲区满时，生产者线程需要等待消费者线程取出数据后再继续放入数据；当缓冲区空时，消费者线程需要等待生产者线程放入数据后再继续取出数据。这就需要使用wait()和notify()方法来实现。
+
+// 定义一个共享的缓冲区类
+class Buffer {
+    private int data; // 存储数据的变量
+    private boolean empty; // 标记缓冲区是否为空
+
+    public Buffer() {
+        this.empty = true; // 初始时缓冲区为空
+    }
+
+    // 同步的放入数据的方法
+    public synchronized void put(int data) {
+        // 如果缓冲区不为空，就等待消费者取出数据
+        while (!empty) {
+            try {
+                wait();//直接调用
+            } catch (InterruptedException e) {//InterruptedException会报中断
+                e.printStackTrace();
+            }
+        }
+        // 放入数据，并设置缓冲区为非空
+        this.data = data;
+        this.empty = false;
+        System.out.println("放入了数据：" + data);
+        // 唤醒消费者线程
+        notify();//直接调用
+    }
+
+    // 同步的取出数据的方法
+    public synchronized int get() {
+        // 如果缓冲区为空，就等待生产者放入数据
+        while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // 取出数据，并设置缓冲区为空
+        int result = this.data;
+        this.empty = true;
+        System.out.println("取出了数据：" + result);
+        // 唤醒生产者线程
+        notify();
+        return result;
+    }
+}
+
+// 定义一个生产者线程类，实现Runnable接口
+class Producer implements Runnable {
+    private Buffer buffer; // 共享的缓冲区对象
+
+    public Producer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    
+    public void run() {
+        // 生产10个数据放入缓冲区
+        for (int i = 1; i <= 10; i++) {
+            buffer.put(i);
+            try {
+                Thread.sleep(100); // 模拟生产时间
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+// 定义一个消费者线程类，实现Runnable接口
+class Consumer implements Runnable {
+    private Buffer buffer; // 共享的缓冲区对象
+
+    public Consumer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    
+    public void run() {
+        // 消费10个数据从缓冲区
+        for (int i = 1; i <= 10; i++) {
+            buffer.get();
+            try {
+                Thread.sleep(100); // 模拟消费时间
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+// 创建并启动生产者和消费者线程
+class TestWaitNotify {
+
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer(); // 创建一个共享的缓冲区对象
+
+        Producer producer = new Producer(buffer); // 创建一个生产者线程对象，并传入缓冲区对象
+        Consumer consumer = new Consumer(buffer); // 创建一个消费者线程对象，并传入缓冲区对象
+
+        Thread producerThread = new Thread(producer); // 创建一个生产者线程，并传入生产者线程对象
+        Thread consumerThread = new Thread(consumer); // 创建一个消费者线程，并传入消费者线程对象
+        
+        producerThread.start(); // 启动生产者线程
+        consumerThread.start(); // 启动消费者线程
     }
 }
